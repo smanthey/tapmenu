46d4cc77b887490433622017683412f5201c67c9
smanthey <scottman@me.com>
2024-10-25 06:30:36 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Top of the Rock Restaurant & Bar</h1>
+      </header>
+      <main>
+        <Menu />
+      </main>
+    </div>
+  );
+}
+
+export default App;
